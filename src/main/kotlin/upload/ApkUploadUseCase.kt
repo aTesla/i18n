72b56364aacfcbd479c25cba2b01dee1d24e997f
LiChunJiang <lcj@huolian.com>
2025-01-23 15:57:56 +0800

    1.0.32

--- src/main/kotlin/upload/ApkUploadUseCase.kt
+++ src/main/kotlin/upload/ApkUploadUseCase.kt
@@ -15,10 +15,10 @@ import java.security.MessageDigest
 import kotlin.concurrent.thread
 
 class ApkUploadUseCase(
-    val versionCode: Long = 31,
-    val versionName: String = "1.0.31",
+    val versionCode: Long = 32,
+    val versionName: String = "1.0.32",
     val is_force_upgrade: Boolean = true,
-    val upgrade_content: String = "• Add IM Chat\n• Minify apk size\n• Fix issues",
+    val upgrade_content: String = "• Fix IM\n• Fix send coin\n• Fix issues",
     val apk_url: String = "https://wisdom-pkg.s3.us-east-1.amazonaws.com/wisdomuae-${versionName}.apk"
 ) {
     val baseUrl = "https://api.wisdom-bank.com"
